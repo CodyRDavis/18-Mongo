0141c934cded594a3e3dec485ab01f46e7eeb5b4
Cody Davis <Cody.1337@gmail.com>
2019-01-05 15:53:34 -0600

    added some ;

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = process.env.URL || '127.0.0.1'
-const port = process.env.PORT || 3000
+const hostname = process.env.URL || '127.0.0.1';
+const port = process.env.PORT || 3000;
 
 const listening = () => console.log(`Server listening at ${hostname}:${port}`);
 
